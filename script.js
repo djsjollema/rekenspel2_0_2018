d6368cdc0213743ba2eb72064e7010b8149f1334
Jelle Sjollema <djsjollema@gmail.com>
2018-03-06 09:03:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const myAssignment = document.getElementById('myAssignment');
+const myInput = document.getElementById('myInput');
+const feedback = document.getElementById('feedback');
+
+let assignments = [];
+let counter = 0;
+
+function init(){
+  //
+}
+
+function inputHandler(evt){
+  //
+}
+
+function makeSum(){
+  //
+}
+
+function evaluate(){
+  
+}
+
+function getNumber(){
+  let number = Math.floor(Math.random()*9)+1;
+  return number;
+}
+
+myInput.addEventListener('keydown',inputHandler,false);
